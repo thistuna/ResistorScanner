494eb5f508a88798e97a73f881ff2c9eb092271e
thistuna <thistuna@gmail.com>
2020-05-28 20:24:12 +0900

    ソース書いた

--- app/src/main/java/com/thistuna/resistorscanner/MainActivity.java
+++ app/src/main/java/com/thistuna/resistorscanner/MainActivity.java
@@ -1,10 +1,24 @@
 package com.thistuna.resistorscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener {
+    private CameraBridgeViewBase m_cameraView;
+    private Mat mOutputFrame;
 
     static {
         System.loadLibrary("opencv_java4");
@@ -14,5 +28,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPermissionCamera(this);
+        OpenCVLoader.initDebug();
+        m_cameraView = findViewById(R.id.camera_view);
+        m_cameraView.setCvCameraViewListener(this);
+        m_cameraView.enableView();
+    }
+
+    public static boolean getPermissionCamera(Activity activity) {
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = new String[]{Manifest.permission.CAMERA};
+            ActivityCompat.requestPermissions(
+                    activity,
+                    permissions,
+                    0);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mOutputFrame = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mOutputFrame.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat inputFrame) {
+        // ここで何らかの画像処理を行う
+        // 試しに、ネガポジ反転してみる
+        Core.bitwise_not(inputFrame, inputFrame);
+
+        return mOutputFrame;
     }
 }
